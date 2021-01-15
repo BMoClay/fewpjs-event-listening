7c4dab781b61c27a7d4fe769dced7a3ff4d86884
BMoClay <benmorgancleveland@gmail.com>
2021-01-15 13:38:51 -0500

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const input = document.getElementById('input');
 
+input.addEventListener('click', function(event) {
+    alert('I was clicked!');
+  });
+
 function addingEventListener() {
     input.addEventListener('click', function(event) {
         return('I was clicked!');
     });
 }
+
+
